5517c64eeea759a24b5a38b53a13d7348842ead8
alter-sachin <guitarplayersachin@gmail.com>
2017-09-03 16:44:08 +0530

    two files. one uses scrapy , the other incomplete

---
+++ ndtvRecipeScrapper.py
@@ -0,0 +1,20 @@
+import scrapy
+
+class NdtvRecipeSpider(scrapy.Spider):
+	name = 'v5rp/la-biryaneez-chalet'
+	start_urls = ['http://food.ndtv.com/recipes/chicken-recipes']
+
+	def parse(self,response):
+		for title in response.css('li.main_image'):
+			link = title.css('a::attr("href")').extract_first().rstrip() 
+			yield scrapy.Request(link, callback=self.parse_attr)
+		for links in response.css('span.pagination'):
+			print(links.css('a::text').extract_first())
+			if(links.css('a::text').extract_first() == 'Next »'):
+				yield response.follow(links.css('a::attr("href")').extract_first(),self.parse)
+
+	def parse_attr(self,response):
+		ingredients = response.css('div.keyword_tag')
+		for ingredient in ingredients.css('a::text'):
+			print(ingredient.extract())
+			yield {'ingredient' : ingredient.extract()}
---
+++ scraper.py
@@ -0,0 +1,21 @@
+﻿import requests
+from bs4 import BeautifulSoup
+
+page = requests.get("http://food.ndtv.com/recipe-sattu-parantha-816921")
+
+soup =  BeautifulSoup(page.content , 'html.parser')
+
+
+#print (soup.prettify())
+
+#print type(soup)
+
+ingredients = soup.find_all("div", class_="ingredients")
+
+final_list = {}
+
+for element in ingredients:
+	final_list = ingredients[0].li.text
+	print(final_list)
+	print(type(final_list))
+	
