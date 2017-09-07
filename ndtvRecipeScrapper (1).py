a806410418f2630f12ccec6799f9e419fd648b62
alter-sachin <guitarplayersachin@gmail.com>
2017-09-07 08:12:20 +0530

    comments

--- ndtvRecipeScrapper (1).py
+++ ndtvRecipeScrapper (1).py
@@ -1,14 +1,14 @@
 import scrapy
 
-class NdtvRecipeSpider(scrapy.Spider):
-	name = 'v5rp/la-biryaneez-chalet'
-	start_urls = ['http://food.ndtv.com/recipes']
+class NdtvRecipeSpider(scrapy.Spider):#scrapy spider object as an argument 
+	name = 'v5rp/la-biryaneez-chalet' 
+	start_urls = ['http://food.ndtv.com/recipes'] #starting url , this page has a list of recipe types 
 
-	def parse(self,response):
-		for title in response.css('li.main_image'):
-			link = title.css('a::attr("href")').extract_first().rstrip()
-			print(link) 
-			yield scrapy.Request(link, callback=self.parse_page)
+	def parse(self,response):      # self , the current instance , needs to be explicitly defined in python 
+		for title in response.css('li.main_image'): #extract css li class main_image , iterate over titles
+			link = title.css('a::attr("href")').extract_first().rstrip() #extract url for title 1 
+			print(link) #check
+			yield scrapy.Request(link, callback=self.parse_page)  #yield 
 
 	def parse_page(self,response):
 		for title in response.css('li.main_image'):
